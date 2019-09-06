148503b396c34c551ecde1cef21b4e08d70f9137
jbritoatec <joao.brito.t0114413@edu.atec.pt>
2019-09-06 23:06:21 +0100

    2 files modified

--- src/app/alunos.ts
+++ src/app/alunos.ts
@@ -1,1 +1,17 @@
-export const alunos = []
+export class aluno {
+name:string;
+age:number;
+  constructor(name:string,age:number){
+    this.name=name;
+    this.age=age;
+  }
+}
+
+export const alunos = [
+  new aluno("Mikas",18),
+  new aluno("Sousa",18),
+  new aluno("Brito",18),
+  new aluno("Tiago",18),
+  new aluno("Daniel",18),
+
+];
